532f5b59fc36418a2537fba0db8ed687b658bbf6
Carlo1891 <carlo.18@live.it>
2022-06-17 16:12:10 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+// Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Gioco dei dadi (separatemente)
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Consigli del giorno:
+// -scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// -javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// -si ma noi cosa vogliamo fare?
+// -torniamo a scrivere in italiano
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
